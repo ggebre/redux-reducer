5b0ba5ddc3abc84cef1def76fd75f980069d046b
Getu Gebre <getu.gebre.gg@gmail.com>
2021-02-15 11:06:36 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    if (action.type == "ADD_FRIEND"){
+        return {friends: [...state.friends, action.friend]}
+    } else if (action.type == "REMOVE_FRIEND"){
+        const filteredFriends = [...state.friends].filter(friend => !(friend.id === action.id))
+        
+        return {friends: filteredFriends}
+    } else {
+        return {friends: [...state.friends]}
+    }
 }
